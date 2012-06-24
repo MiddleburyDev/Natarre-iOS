b7889d9a6dc0df7081dcc730af7c3a89837c6ea6
Nate Beatty <nate.beatty@gmail.com>
2012-06-24 03:37:06 -0400

    New Icons

---
+++ Natarre/TNBThisWeekViewController.h
@@ -0,0 +1,13 @@
+//
+//  TNBThisWeekViewController.h
+//  Natarre
+//
+//  Created by Thomas Beatty on 6/24/12.
+//  Copyright (c) 2012 Nate Beatty. All rights reserved.
+//
+
+#import "TNBPullToRefreshTableViewController.h"
+
+@interface TNBThisWeekViewController : TNBPullToRefreshTableViewController
+
+@end
---
+++ Natarre/TNBThisWeekViewController.m
@@ -0,0 +1,43 @@
+//
+//  TNBThisWeekViewController.m
+//  Natarre
+//
+//  Created by Thomas Beatty on 6/24/12.
+//  Copyright (c) 2012 Nate Beatty. All rights reserved.
+//
+
+#import "TNBThisWeekViewController.h"
+
+@interface TNBThisWeekViewController ()
+
+@end
+
+@implementation TNBThisWeekViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
